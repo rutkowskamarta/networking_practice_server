e666cc11a7333efb278dacbbef32d71d31ddb391
Lady Marta <lady.marta@theknightsofunity.com>
2022-02-08 21:38:17 +0100

    Implementing first version of server connection.

---
+++ Server/Plugins/ServerPlugin/ServerPlugin/ServerPlugin.cs
@@ -0,0 +1,28 @@
+﻿using DarkRift.Server;
+using System;
+
+namespace ServerPlugin
+{
+	public class ServerPlugin : Plugin
+	{
+		public override bool ThreadSafe => false;
+
+		public override Version Version => new Version(0, 0, 1);
+
+		public ServerPlugin(PluginLoadData pluginLoadData) : base(pluginLoadData)
+		{
+			ClientManager.ClientConnected += OnClientConnected;
+			ClientManager.ClientDisconnected += OnClientDisonnected;
+		}
+
+		private void OnClientConnected(object sender, ClientConnectedEventArgs e)
+		{
+			Logger.Log("client connected " + e.Client.ID, DarkRift.LogType.Info);
+		}
+
+		private void OnClientDisonnected(object sender, ClientDisconnectedEventArgs e)
+		{
+			Logger.Log("client disconnected " + e.Client.ID, DarkRift.LogType.Info);
+		}
+	}
+}
