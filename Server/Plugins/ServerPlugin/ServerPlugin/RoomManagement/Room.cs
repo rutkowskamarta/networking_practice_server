b386367b2f6c12bf788e9d48b029e2136c2fb9b5
Lady Marta <lady.marta@theknightsofunity.com>
2022-02-17 22:26:07 +0100

    WIP room management.

---
+++ Server/Plugins/ServerPlugin/ServerPlugin/RoomManagement/Room.cs
@@ -0,0 +1,50 @@
+﻿using ServerPlugin.PlayerManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerPlugin.RoomManagement
+{
+	class Room
+	{
+		private const int RoomIdLenght = 6;
+		private Dictionary<string, Player> players;
+
+		public string ID { get; private set; }
+
+		public Room()
+		{
+			ID = GenerateRandomId();
+			players = new Dictionary<string, Player>();
+		}
+
+		public void JoinRoom(Player player)
+		{
+
+		}
+
+		public void LeaveRoom(Player player)
+		{
+
+		}
+
+		private string GenerateRandomId()
+		{
+			StringBuilder stringBuilder = new StringBuilder();
+			Random random = new Random();
+
+			char letter;
+
+			for (int i = 0; i < RoomIdLenght; i++)
+			{
+				double asciiCode = random.NextDouble();
+				int shift = Convert.ToInt32(Math.Floor(25 * asciiCode));
+				letter = Convert.ToChar(shift + 65);
+				stringBuilder.Append(letter);
+			}
+			return stringBuilder.ToString();
+		}
+	}
+}
---
+++ Server/Plugins/ServerPlugin/ServerPlugin/RoomManagement/RoomManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace ServerPlugin.RoomManagement
+{
+	static class RoomManager
+	{
+		private static Dictionary<string, Room> createdRooms = new Dictionary<string, Room>();
+
+		public static void AddRoom(Room room)
+		{
+			createdRooms.Add(room.ID, room);
+		}
+
+		public static void RemoveRoom(Room room)
+		{
+			createdRooms.Add(room.ID, room);
+		}
+
+		public static void GetRoomOfID(string ID)
+		{
+
+		}
+	}
+}
