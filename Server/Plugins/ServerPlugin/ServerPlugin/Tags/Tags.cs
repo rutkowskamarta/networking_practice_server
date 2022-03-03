3e99962cee3f2ee857f04b2dc8009fc0573e79e8
Lady Marta <lady.marta@theknightsofunity.com>
2022-03-03 22:07:52 +0100

    Adding new tags.

--- Server/Plugins/ServerPlugin/ServerPlugin/Tags/Tags.cs
+++ Server/Plugins/ServerPlugin/ServerPlugin/Tags/Tags.cs
@@ -8,6 +8,17 @@
         public static readonly ushort LeaveRoomRequest = 2;
         public static readonly ushort CreateRoomRequest = 3;
 
+        public static readonly ushort StartGame = 4;
+
+        public static readonly ushort AddCategory = 5;
+        public static readonly ushort RemoveCategory = 6;
+        public static readonly ushort SetRoundsNumber = 7;
+        public static readonly ushort PlayerReady = 8;
+        public static readonly ushort ContinueToGame = 9;
+        public static readonly ushort StopTheGame = 10;
+        public static readonly ushort SubmitTheAnswers = 11;
+
+
         //response tags
         public static readonly ushort CreateRoomResponseSucess = 100;
         public static readonly ushort CreateRoomResponseFail = 101;
